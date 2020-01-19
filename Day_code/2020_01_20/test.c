5f780d4111e3a93ddc92c41c8321398d2affac8f
Y-Leo <964952155@qq.com>
2020-01-20 00:04:34 +0800

    练习

---
+++ Day_code/2020_01_20/test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 1;
+    int b = 2;
+    for(; a < 8; a++)
+    {
+        b += a;
+        a += 2;
+    }
+    printf("%d,%d\n",a,b);
+    return 0;
+}
