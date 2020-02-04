b3508ffc010dfdc199d3adf0966f48fedcee4352
Y-Leo <964952155@qq.com>
2020-02-05 00:30:35 +0800

    练习

---
+++ Day_code/2020_01_16/test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+struct HAR
+{
+    int x,y;
+    struct HAR *p;
+}h[2];
+
+int main()
+{
+    h[0].x = 1; h[0].y = 2;
+    h[1].x = 3; h[1].y = 4;
+    h[0].p = &h[1];h[1].p = h;
+    printf("%d,%d \n",(h[0].p)->x,(h[1].p)->y);
+    return 0;
+}
