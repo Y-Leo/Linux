2e5f25cfb53592ce4b6681c43540032a28dd83ae
Y-Leo <964952155@qq.com>
2020-02-08 00:05:35 +0800

    练习

---
+++ Day_code/2020_02_08/test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    scanf("%d,%d",&a,&b);
+    if(a>b)
+    {
+        a = b;
+        b = a;
+    }
+    else
+        a++;
+    b++;
+    printf("%d,%d",a,b);
+    return 0;
+}
