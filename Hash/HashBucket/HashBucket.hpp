4de21dce19e3b779d8c1d8d8b464ed6f86817f31
Y-Leo <964952155@qq.com>
2020-03-16 14:56:45 +0800

    完善哈希桶，增加迭代器

--- Hash/HashBucket/HashBucket.hpp
+++ Hash/HashBucket/HashBucket.hpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include "Common.hpp"
+using namespace std;
 
 template <class T>
 struct HashNode
@@ -15,15 +16,108 @@ struct HashNode
     T data_;
 };
 
+template<class T, class DF = DFDef<T>>
+class HashBucket;
+
+//迭代器:从事实现原理上分类----1、原生态指针  2、对原生态的指针进行封装
 template<class T,class DF = DFDef<T>>
+struct HBIterator
+{
+    typedef HashNode<T> Node;
+    typedef HBIterator<T> Self;
+
+    HBIterator(Node* pNode = nullptr,HashBucket<T,DF>* ht = nullptr)
+        :_pNode(pNode)
+         ,_ht(ht)
+    {}
+
+    //具有类似指针的操作
+    T& operator*()
+    {
+        return _pNode->data_;
+    }
+
+    T* operator->()
+    {
+        return &(_pNode->data_);
+    }
+
+    //能够移动
+    //前置++
+    Self& operator++()
+    {
+        Next();
+        return *this;
+    }
+
+    //后置++
+    Self operator++(int)
+    {
+        Self temp(*this);
+        Next();
+        return temp;
+    }
+
+    //比较
+    bool operator!=(Self& s)
+    {
+        return _pNode != s._pNode;
+    }
+
+    bool operator==(Self& s)
+    {
+        return _pNode == s._pNode;
+    }
+
+    void Next()
+    {
+        if(_pNode->pNext_)
+        {
+            //_pNode对应的链表后续还有节点
+            _pNode = _pNode->pNext_;
+        }
+        else
+        {
+            //pNode是当前链表中的最后一个节点
+            //找pNode后序第一个非空桶
+            //问题：如何知道pNode在哪个桶中？解决：如果知道哈希函数，就能直到pNode的桶号
+            //如果能够拿到当前迭代器对应的哈希表，就可以拿到哈希函数
+            size_t bucketNo = _ht->HashFunc(_pNode->data_) + 1;
+            for(; bucketNo < _ht->table_.capacity(); ++bucketNo)
+            {
+                if(_ht->table_[bucketNo])
+                {
+                    //非空桶已找到
+                    _pNode = _ht->table_[bucketNo];
+                    return;
+                }
+            }
+            //已经是末尾位置
+            _pNode = nullptr;
+        }
+    }
+    
+    HashNode<T>* _pNode;
+    HashBucket<T,DF>* _ht;
+};
+
+
+//迭代器如何与类结合:
+//1、为该类定义迭代器
+//2、在类中重新给迭代器命名
+//3、增加begin/end
+template<class T,class DF>
 class HashBucket
 {
-public:
+    friend struct HBIterator<T,DF>;
     typedef HashNode<T> Node;
+    typedef HBIterator<T,DF> iterator;
+
+public:
     HashBucket(size_t capacity = 10)
         :size_(0)
     {
-        table_.resize(10 /*GetNextPrime(10)*/);
+        table_.resize(/*GetNextPrime(10)*/10);
     }
 
     bool insertunique(const T& data)
@@ -98,6 +192,24 @@ bool empty()const
     return 0 == size_;
 }
 
+iterator begin()
+{
+    //找第一个非空桶
+    for(size_t bucket = 0; bucket < table_.capacity(); ++bucket)
+    {
+        if(table_[bucket])
+            return iterator(table_[bucket],this);
+    }
+
+    return end();
+}
+
+iterator end()
+{
+    return iterator(nullptr,this);
+}
+
+//测试
 void PrintHashBucket()
 {
     for(size_t bucketNo = 0;bucketNo < table_.capacity();++bucketNo)
@@ -145,6 +257,17 @@ void TestHashBucket()
 
     ht.insertunique(44);
     ht.insertunique(54);
+
+    /*
+    auto it = ht.begin();
+    while(it != ht.end())
+    {
+        cout << *it << " ";
+        ++it;
+    }
+    cout << endl;
+*/
+
     ht.PrintHashBucket();
 
     ht.eraseunique(44);
